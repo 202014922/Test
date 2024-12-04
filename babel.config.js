d8bd506286dc4d38c1467eb5a8bd3fe9741a4553
Psw <ikaros364125@kyonggi.ac.kr>
2024-12-05 01:10:25 +0900

    twentyth

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  presets: [
-    'react-app', // Create React App에서 기본으로 제공하는 프리셋, React 프로젝트에서 기본 설정으로 많이 사용
-    '@babel/preset-env' // 최신 JavaScript 문법을 트랜스파일링
-  ],
-};
+    presets: [
+      '@babel/preset-env', // 최신 JavaScript 문법을 변환
+      '@babel/preset-react' // React JSX 문법을 변환
+    ],
+  };
+  
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-    // node_modules에서 axios를 변환하도록 설정
     transformIgnorePatterns: [
-      "/node_modules/(?!axios)/" // axios는 ES 모듈을 사용하므로 처리해야 함
+      "/node_modules/(?!axios)/"  // axios 모듈을 변환 대상으로 지정
     ],
-    // 필요한 경우 추가적인 Jest 설정을 추가할 수 있습니다.
-    testEnvironment: 'jsdom', // React 환경에서 테스트 실행을 위한 설정
   };
   
